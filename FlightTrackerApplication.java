91931123686dc90d0e5eab8799ea8d8bdf4448da
Younesbhb <122587727+Younesbhb@users.noreply.github.com>
2024-03-25 02:50:25 -0400

    Update FlightTrackerApplication.java

--- FlightTrackerApplication.java
+++ FlightTrackerApplication.java
@@ -2,20 +2,7 @@ import java.util.List;
 
 public class FlightTrackerApplication {
     public static void main(String[] args) {
-        FlightSearchController searchController = new FlightSearchController();
-        //User registeredUser = new User("1","Kevin",true);
-        //User nonRegisteredUser = new User("2","David",false);
-
-        // Example search
-        String source = "Montreal";
-        String destination = "London";
-
-        System.out.println("Registered user's view:");
-        //List<Flight> resultsForRegistered = searchController.searchFlights(source, destination, registeredUser);
-        //esultsForRegistered.forEach(flight -> System.out.println(flight.getFlightNumber() + " from " + flight.getSource() + " to " + flight.getDestination() + ", Airline: " + flight.getAirlineName() + ", Aircraft: " + flight.getAircraftType()));
-
-        System.out.println("\nNon-registered user's view:");
-        //List<Flight> resultsForNonRegistered = searchController.searchFlights(source, destination, nonRegisteredUser);
-        //resultsForNonRegistered.forEach(flight -> System.out.println(flight.getFlightNumber() + " from " + flight.getSource() + " to " + flight.getDestination()));
+        Console console = new Console();
+        console.launchApplication();
     }
 }

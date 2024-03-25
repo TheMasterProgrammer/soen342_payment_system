0214eaf16bf7b87b86c250c6e290f347b135316c
Younesbhb <122587727+Younesbhb@users.noreply.github.com>
2024-03-25 02:52:52 -0400

    Update SystemAdministrator.java

--- SystemAdministrator.java
+++ SystemAdministrator.java
@@ -1,9 +1,11 @@
 public class SystemAdministrator extends User {
-    public SystemAdministrator(String id, String name) {
-        this.id = id;
-        this.name = name;
+    public SystemAdministrator(String username, String password) {
+        this.id = String.valueOf(counter++);
+        this.username = username;
+        this.password = password;
         this.isRegistered = true;
     }
 
-    // Additional methods specific to system administrators
+
+
 }

0c18c17ddf3505ad1fa7e5220985402585005a7a
Younesbhb <122587727+Younesbhb@users.noreply.github.com>
2024-03-25 02:52:33 -0400

    Update PrivateFlight.java

--- PrivateFlight.java
+++ PrivateFlight.java
@@ -1,7 +1,12 @@
 import java.time.LocalDateTime;
 
 public class PrivateFlight extends Flight{
-    public PrivateFlight(String flightNumber, String sourceAirportCode, String destinationAirportCode, Aircraft aircraft, LocalDateTime actualDepartureTime, LocalDateTime scheduledDepartureTime, LocalDateTime actualArrivalTime, LocalDateTime scheduledArrivalTime) {
-        super(flightNumber, sourceAirportCode, destinationAirportCode, aircraft, actualDepartureTime, scheduledDepartureTime, actualArrivalTime, scheduledArrivalTime);
+    public PrivateFlight(String flightNumber, Airport sourceAirport, Airport destinationAirport, Aircraft aircraft, LocalDateTime actualDepartureTime, LocalDateTime scheduledDepartureTime, LocalDateTime actualArrivalTime, LocalDateTime scheduledArrivalTime) {
+        super(flightNumber, sourceAirport, destinationAirport, aircraft, actualDepartureTime, scheduledDepartureTime, actualArrivalTime, scheduledArrivalTime);
+    }
+
+    //Displays flight info for a private flight
+    public String displayFlightInfo(){
+        return "Private " + super.displayFlightInfo();
     }
 }

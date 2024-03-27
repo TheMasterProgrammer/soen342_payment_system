3a8465555cbd5ab0e77d5d9d1a34f02974aa6eb2
Younesbhb <122587727+Younesbhb@users.noreply.github.com>
2024-03-27 19:23:58 -0400

    Update UserCatalog.java

--- UserCatalog.java
+++ UserCatalog.java
@@ -10,6 +10,25 @@ public class UserCatalog {
     private List<User> users;
     private UserCatalog() {
         users = new ArrayList<>();
+        //Some hardcoded data
+
+
+        //Registering a system admin
+        users.add(new SystemAdministrator("SystemEmployee","123"));
+        //Registering an airline admin for Air Canada
+        users.add(new AirlineAdministrator("AirCanadaEmployee","123",AirlineCatalog.getAirlineCatalogInstance().getAirlines().get(0)));
+        //Registering an airline admin for Fedex
+        users.add(new AirlineAdministrator("FedexEmployee","123",AirlineCatalog.getAirlineCatalogInstance().getAirlines().get(1)));
+        //Registering an airport admin for Montreal
+        users.add(new AirportAdministrator("MontrealAdmin","123", AirportCatalog.getAirportCatalogInstance().getAirports().get(0)));
+        //Registering an airport admin for Toronto
+        users.add(new AirportAdministrator("TorontoAdmin","123", AirportCatalog.getAirportCatalogInstance().getAirports().get(1)));
+        //Registering an airport admin for Ottawa
+        users.add(new AirportAdministrator("OttawaAdmin","123", AirportCatalog.getAirportCatalogInstance().getAirports().get(2)));
+        //Registering an airport admin for Austin
+        users.add(new AirportAdministrator("AustinAdmin","123", AirportCatalog.getAirportCatalogInstance().getAirports().get(3)));
+        //Registering a normal user
+        users.add(new NormalUser("NormalUser","normal"));
     }
     public static UserCatalog getCredentialsCatalogInstance(){
         if(userCatalog==null){

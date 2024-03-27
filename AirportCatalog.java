4e51ac356cf6d6f73f29fd8c3e68904e055578bf
Younesbhb <122587727+Younesbhb@users.noreply.github.com>
2024-03-27 19:26:04 -0400

    Update AirportCatalog.java

--- AirportCatalog.java
+++ AirportCatalog.java
@@ -6,6 +6,12 @@ public class AirportCatalog {
     private List<Airport> airports;
     private AirportCatalog(){
         airports = new ArrayList<>();
+        //Some hardcoded data
+
+        airports.add(new Airport("YUL","Montréal-Pierre Elliott Trudeau International Airport", CityCatalog.getCityCatalogInstance().getCities().get(0))); // Registering an airport in Montreal
+        airports.add(new Airport("YYZ","Toronto Pearson International Airport", CityCatalog.getCityCatalogInstance().getCities().get(1))); // Registering an airport in Toronto
+        airports.add(new Airport("YOW","Ottawa International Airport", CityCatalog.getCityCatalogInstance().getCities().get(2))); // Registering an airport in Ottawa
+        airports.add(new Airport("AUS","Austin-Bergstrom International Airport", CityCatalog.getCityCatalogInstance().getCities().get(3))); // Registering an airport in Austin
     }
     public static AirportCatalog getAirportCatalogInstance(){
         if(airportCatalog==null){
